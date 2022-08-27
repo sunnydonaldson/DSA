095cd2c813d9efe063d9a6d67ab9898e026f8c17
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2022-08-27 15:01:13 +0100

    Add get and set functions for array ADT.

--- Arrays/arrayADT.c
+++ Arrays/arrayADT.c
@@ -11,8 +11,8 @@
 
 typedef struct Array {
   int *arr;
-  int size;
-  int length;
+  size_t size;
+  size_t length;
 } Array;
 // Side note
 // typedef struct Array Array does the same as above
@@ -20,12 +20,15 @@ typedef struct Array {
 
 // Maybe refactor into header file...
 void fillValues(Array *arr, int value);
-Array *instantiate(int size);
+Array *instantiate(size_t size);
 void printArray(Array *arr);
 void append(Array *arr, int value);
+void freeArray(Array **arr);
+int get(Array *arr, size_t idx);
+void set(Array *arr, size_t idx, int value);
 
 // Initializes an instance of Array on the heap
-Array *instantiate(int size)
+Array *instantiate(size_t size)
 {
   Array *array = malloc(sizeof(Array));
   array->arr = malloc(size * sizeof(int));
@@ -37,15 +40,15 @@ Array *instantiate(int size)
 
 void fillValues(Array *arr, int value)
 {
-  for (int i = 0; i < arr->size; i++) {
+  for (size_t i = 0; i < arr->size; i++) {
     arr->arr[i] = value;
   }
 }
 
 void printArray(Array *arr)
 {
-  for (int i = 0; i < arr->length; i++) {
-    printf("[%d]= %d\n", i, arr->arr[i]);
+  for (size_t i = 0; i < arr->length; i++) {
+    printf("[%zu]= %d\n", i, arr->arr[i]);// %zu for size_t
   }
 }
 
@@ -62,6 +65,18 @@ void freeArray(Array **arr)
   free((*arr)->arr);
   free(*arr);
   *arr = NULL;
+}
+
+int get(Array *arr, size_t idx)
+{
+  assert(idx < arr->length);
+  return arr->arr[idx];
+}
+
+void set(Array *arr, size_t idx, int value)
+{
+  assert(idx < arr->length);
+  arr->arr[idx] = value;
 }
 
 int main() {
