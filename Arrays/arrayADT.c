6f3b24a83270e2ca80907f3f073d4e5d639225d6
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2022-08-28 10:17:30 +0100

    Add improved linear search functions for array ADT.

--- Arrays/arrayADT.c
+++ Arrays/arrayADT.c
@@ -29,7 +29,10 @@ void append(Array *arr, int value);
 void freeArray(Array **arr);
 int get(Array *arr, size_t idx);
 void set(Array *arr, size_t idx, int value);
-int *search(Array *arr, int value);
+int *search(Array *arr, int key);
+int *transposeSearch(Array *arr, int key);
+int *moveToHeadSearch(Array *arr, int key);
+static void swap(int *a, int *b);
 void insert(Array *arr, size_t idx, int value);
 int delete(Array *arr, size_t idx);
 
@@ -86,12 +89,57 @@ void set(Array *arr, size_t idx, int value)
 }
 
 // Returns the address of the element if found, else NULL.
-int *search(Array *arr, int value)
+// There are a couple of ways that this function can be improved.
+// Similarly to CPU caching, we can apply the rule of temporal locality,
+// Which states that something that has been accessed recently, will likely
+// need to be accessed again.  So we can improve the linear search by,
+// moving the found key closer to the start of the array, so it gets found
+// faster in future, instead of having to traverse the whole thing.
+int *search(Array *arr, int key)
 {
   for (size_t i = 0; i < arr->length; i++)
-    if (arr->arr[i] == value)
+    if (arr->arr[i] == key)
       return &(arr->arr[i]);
   return NULL;
+}
+// The first way of improving the linear search is called transposition.
+// When we find the key we want, we swap it with the element to its left.
+// So, every time we search for the same key, it gets moved one position
+// closer to the head of the array; eventually it'll get there.
+int *transposeSearch(Array *arr, int key)
+{
+  for (size_t i = 0; i < arr->length; i++) {
+    if(arr->arr[i] == key) {
+      if (i > 0)
+        swap((&arr->arr[i]), &(arr->arr[i-1]));
+        return &(arr->arr[i-1]);
+    }
+  }
+  return NULL;
+}
+
+// The second way of improving the linear search is where you
+// swap the desired key with the element at the start of the array.
+// This has a much more rapid improvement,
+// but I can imagine a shortcoming being, when you swap 2
+// commonly searched for elements, so 1 always ends up being
+// near the end of the array.
+int *moveToHeadSearch(Array *arr, int key)
+{
+  for (size_t i = 0; i < arr->length; i++) {
+    if(arr->arr[i] == key) {
+      swap(&(arr->arr[i]), &(arr->arr[0]));
+      return arr;
+    }
+  }
+  return NULL;
+}
+
+static void swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
 
 void insert(Array *arr, size_t idx, int value)
@@ -147,6 +195,13 @@ int main() {
   delete(array, 2);
   printArray(array);
   // printf("delete at 99 (should fail)\n");
+  printf("\n search with transposition\n");
+  transposeSearch(array, 44);
+  printArray(array);
+
+  printf("\n search with move to head\n");
+  moveToHeadSearch(array, 22);
+  printArray(array);
 
   freeArray(&array);
   return 0;
