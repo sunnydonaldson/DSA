e12ddc1cac78f0659709f46f1570c433883d8f9b
Sunny Donaldson <sunnydonaldson1@gmail.com>
2022-10-28 17:17:04 +0100

    Add basic adjacency list explanation, and start of example (broken).

--- Graphs/graph-representation.c
+++ Graphs/graph-representation.c
@@ -8,6 +8,18 @@
 * 4. Edge List
 ***********************************************************/
 
+typedef struct AdjListNode {
+    int value;
+    AdjListNode *next;
+} AdjListNode;
+
+typedef struct AdjList {
+    AdjListNode *head;
+} AdjList;
+
+AdjList *instantiateGraph(size_t size);
+// Note, these should be moved to their own LinkedList file
+
 void adjacencyMatrix()
 {
     /*An adjacency matrix for an unweighted graph with 4 nodes looks like this.
@@ -45,6 +57,30 @@ void adjacencyMatrix()
     };
 
     printf("Weighted connection between node 2, and 1? %d\n", weightedAdjacencyMatrix[2][1]);
+}
+
+void adjacencyList()
+{
+    /*
+    An adjacency list, is a list of linked lists containing all the edges for each vertex.
+    [
+        0->[1]->[3],
+        1->[3]->[4],  
+        2->[4]->[0],
+        3->[],
+        4->[1],
+    ]
+    */
+    AdjList *adjacencyList = instantiateGraph(5);
+        // The value can be any arbitrary integer in this case
+}
+
+AdjList *instantiateGraph(size_t size)
+{
+    AdjList *adjacencyList = malloc(size * sizeof(AdjList));
+    for (int i = 0; i < size; i++)
+        adjacencyList[i].head = NULL;
+    return adjacencyList;
 }
 
 int main()
