48633773a1f99f0ddfdb613b5dd3042ae1315c1b
sunnydonaldson <sunnydonaldson1@gmail.com>
2022-12-05 21:07:20 +0000

    Complete find duplicates in unsorted array challenge.

--- c/Arrays/array-challenges.c
+++ c/Arrays/array-challenges.c
@@ -29,9 +29,10 @@ void findMissingElementsUnorderedSequence(Array *sequence)
   for (size_t i = smallest; i < bitset->length; i++)
     if (bitset->arr[i] == FALSE)
         printf("%zu not in sequence\n", i);
+  freeArray(&bitset);
 }
 
-findDuplicatesInSortedArray(Array *array)
+void findDuplicatesInSortedArray(Array *array)
 {
     printf("\nfind duplicates in sorted array\n");
     assert(array->length >= 1);
@@ -44,6 +45,21 @@ findDuplicatesInSortedArray(Array *array)
             i = j - 1;
         }
     }
+}
+
+void findDuplicatesInUnsortedArray(Array *input)
+{
+    assert(input->length >= 1);
+    printf("\nfind duplicates in un-sorted array.\n");
+    Array *table = instantiate(max(input) +1);
+    fillArray(table, 0);
+
+    for (size_t i = 0; i < input->length; i++)
+        table->arr[input->arr[i]]++;
+    for (int i = min(input); i < table->length; i++)
+        if (table->arr[i] > 1)
+            printf("%d appeared %d times\n", i, table->arr[i]);
+    freeArray(&table);
 }
 
 int main()
@@ -67,7 +83,17 @@ int main()
     sortedWithDuplicates->arr[9] = 8;
     printArray(sortedWithDuplicates);
     findDuplicatesInSortedArray(sortedWithDuplicates);
+    freeArray(&sortedWithDuplicates);
 
+    Array *unsortedWithDuplicates = instantiate(10);
+    fillArray(unsortedWithDuplicates, 4);
+    unsortedWithDuplicates->arr[0] = 8;
+    unsortedWithDuplicates->arr[1] = 9;
+    unsortedWithDuplicates->arr[5] = 9;
+    unsortedWithDuplicates->arr[6] = 8;
+    unsortedWithDuplicates->arr[7] = 7;
 
+    findDuplicatesInUnsortedArray(unsortedWithDuplicates);
+    freeArray(&unsortedWithDuplicates);
     return 0;
 }
