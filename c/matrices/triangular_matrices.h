ddee9c7040353397c8e0288cae11eba0ec4a7afe
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-01-10 19:34:20 +0000

    Start explanation of lower triangular matrices.

--- c/matrices/triangular_matrices.h
+++ c/matrices/triangular_matrices.h
@@ -4,6 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+*****************************lower triangular matrix****************************
+* For a matrix to be a lower triangular matrix, every element at [i][j]
+* where i >= j must be non-zero.
+* Conversely, every element at [i][j] where j > i must be 0;
+*
+*  j 1 2 3 4 5 
+* i  ---------
+* 1| 1 0 0 0 0
+* 2| 2 3 0 0 0
+* 3| 3 6 8 0 0 
+* 4| 1 4 7 8 0
+* 5| 2 5 7 9 1
+* 
+*  j 1 2 3 4 5 
+* i  ---------
+* 1|
+* 2|
+* 3|
+* 4|
+* 5|
+**/
+
 typedef struct LowerTriangularMatrix {
   int *arr;
   size_t size;
