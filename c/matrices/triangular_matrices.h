3f45ec3b39d19884775d5377e9fdbe8f4678c191
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-04-23 12:20:21 +0100

    Add explanation of lower triangular matrices.

--- c/matrices/triangular_matrices.h
+++ c/matrices/triangular_matrices.h
@@ -26,6 +26,20 @@
 * 3|
 * 4|
 * 5|
+* 
+* There's no need to waste space by storing all the 0s, when we know that everything where j > i is 0.
+* We can just use a 1d array to store all the non-zero values, and use a formula to look them up.
+*
+* The lower triangular matrix above can be represented like:
+* [1, 2, 3, 3, 6, 8, 1, 4, 7, 8, 2, 5, 7, 9, 1]
+*
+* The size of this array can be found by calculating the n + (n-1), + (n-2), ..., 0
+* There's a cool trick to do this: size = n * (n + 1)  / 2
+* 
+* We can look up a value in this 1d array by using the formula:
+*   (i * (i -1) / 2) + ( j - 1)
+* The first term finds the correct row, and the second term finds the correct column.
+*
 **/
 
 typedef struct LowerTriangularMatrix {
