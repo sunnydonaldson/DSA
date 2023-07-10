e6cc6bba9eacfa59e0d0c466fac021ba220c1e33
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2023-07-10 21:31:33 +0100

    Start implementing RestoreAvl for AvlInsert.

--- c/trees/BST/AVL/avl_tree.c
+++ c/trees/BST/AVL/avl_tree.c
@@ -9,13 +9,29 @@ Calcualte the balance factor for all nodes:
 **/
 
 static int BalanceFactor(BSTNode *root);
+static void RestoreAvl(BSTNode *);
 
 void AvlInsert(BSTNode *root, BSTNode *node) {
   assert(root && node);
-  InsertNode(root, node);
+  InsertNode(root, node, &RestoreAvl);
 
 }
 
+static void RestoreAvl(BSTNode * node) {
+  int balance_factor = BalanceFactor(node);
+  if (balance_factor <= 1 && balance_factor >= -1) {
+    return;
+  }
+
+  if (balance_factor < -1) {
+    // rotate_left
+  } else {
+    // rotate_right
+  }
+}
+
+/** Returns negative if right subtree taller, positive if right shorter, else 0. **/
 static int BalanceFactor(BSTNode *root) {
   return (int)Height(root->left) - (int)Height(root->right);
 }
+
